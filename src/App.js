b5d1705463b9523a851c028baaeef82a3cb5044a
somyakant9 <msomya143@gmail.com>
2022-05-17 06:26:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import styles from './App.module.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ={styles.system}>
+     <h1>Mobile Operating System</h1>
+     <ul> <li>Android</li>
+        <li>Blackberry</li>
+        <li>I Phone</li>
+        <li>Window's Phone</li>
+        </ul> 
+       
+     <h1> Mobile Manufacturers</h1>
+     <ul><li>Samsung</li>
+     <li>HTC</li>
+     <li>Micromax</li>
+     <li>Apple</li>
+     </ul>
     </div>
   );
 }
